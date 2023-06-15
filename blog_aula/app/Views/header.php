00f208ca69779643294480b13cc97c59e5c33189
vininirs <134555196+vininirs@users.noreply.github.com>
2023-06-14 23:00:28 -0400

    Add files via upload

---
+++ blog_aula/app/Views/header.php
@@ -0,0 +1,26 @@
+<header class="bg-dark">
+    <div class="container">
+        <nav class="navbar navbar-expand-sm navbar-dark">
+            <a class="navbar-brand" href="<?=URL?>">LOGO</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?=URL?>" data-tooltip="tooltip" title="Página Inicial">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?=URL?>/paginas/sobre" data-tooltip="tooltip" title="Sobre nós">Sobre nós</a>
+                    </li>
+                </ul>
+<span class="navbar-text">
+<form class="container-fluid justify-content-start">
+    <button class="btn btn-primary" type="button">Entrar</button>
+    <button class="btn btn-primary" type="button">Cadastre-se </button>
+  </form>
+                            </div>
+        </nav>
+    </div>
+</header>
---
+++ blog_aula/app/configuracao.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Arquivo de configuração
+ */
+
+// __FILE__  — Constante Mágica. Retorna o caminho completo e o nome do arquivo
+// dirname — Retorna o caminho/path do diretório pai
+
+//define e const — Define uma constante. As constantes não podem ser alteradas depois de declaradas.
+define('APP', dirname(__FILE__));
+
+define('URL','http://localhost/framework');
+
+define('APP_NOME','Curso de PHP7 Orientado a Objetos e MVC');
+
+const APP_VERSAO = '1.0.0';
